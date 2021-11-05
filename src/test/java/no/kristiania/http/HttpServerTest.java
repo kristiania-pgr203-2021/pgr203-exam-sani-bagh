f79ace82b9c37f7fcc384d7a4fbf2ab6a071bfce
Saniya Baghirova <saniya.ibraimova@gmail.com>
2021-11-05 11:16:40 +0100

    Tests shouldListQuestions 🟢

--- src/test/java/no/kristiania/http/HttpServerTest.java
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -1,7 +1,8 @@
 package no.kristiania.http;
 
+import no.kristiania.survey.Question;
 import no.kristiania.survey.QuestionDao;
-import no.kristiania.survey.SurveyDao;
+import no.kristiania.survey.QuestionDaoTest;
 import no.kristiania.survey.TestData;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,6 @@ import java.sql.SQLException;
 import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpServerTest {
@@ -62,4 +62,20 @@ public class HttpServerTest {
     }
 
 
+    @Test
+    void shouldListQuestions() throws SQLException, IOException {
+        QuestionDao questionDao = new QuestionDao(TestData.testDataSource());
+        server.addController("/api/questions", new ListQuestionsController(questionDao));
+
+        Question question1 = QuestionDaoTest.exampleQuestion();
+        Question question2 = QuestionDaoTest.exampleQuestion();
+        questionDao.save(question1);
+        questionDao.save(question2);
+
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/api/questions");
+        assertThat(client.getMessageBody())
+                .contains("<h4>" + question1.getTitle() + "</h4>" + "\n" +  "<h4>" + question1.getText() + "</h4>")
+                .contains("<h4>" + question2.getTitle() + "</h4>" + "\n" +  "<h4>" + question2.getText() + "</h4>")
+        ;
+    }
 }
