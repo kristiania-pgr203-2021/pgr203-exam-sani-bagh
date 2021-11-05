c29b4d83c2d6c364b242c6c4f99423ee201653fd
Marina1014 <makh004@student.kristiania.no>
2021-11-05 14:00:46 +0100

    db change

--- src/main/resources/db/migration/V003__create_table_answerAlternatives.sql
+++ src/main/resources/db/migration/V003__create_table_answerAlternatives.sql
@@ -1,5 +1,6 @@
 create table answerAlternatives (
     answer_id serial primary key,
     answer_text varchar(200) not null,
-    questionID int references question(question_id)
+    questionID int references question(question_id),
+    survey_id int references survey(survey_id);
 )
--- src/main/resources/db/migration/V005__create_table_userAnswer.sql
+++ src/main/resources/db/migration/V005__create_table_userAnswer.sql
@@ -1,5 +1,6 @@
 create table userAnswer (
     userID int references user(user_id),
     questionID int references question(question_id),
-    answerID int references answerAlternatives(answer_id)
+    answerID int references answerAlternatives(answer_id),
+    survey_id int references survey(survey_id);
 )
